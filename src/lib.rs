2e72bdc308648894fc96027ce28595fbbbcbbc10
Caspar Krieger <git@asparck.com>
2023-07-26 15:48:23 +0800

    Add another triangle to make shape more complex

--- src/lib.rs
+++ src/lib.rs
@@ -92,12 +92,14 @@ mod unformatted {
         Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] },
         Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 0.5] },
         Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.5] },
+        Vertex { position: [0.7, -0.6, 0.0], color: [0.2, 1.0, 0.5] },
     ];
 
     pub const INDICES: &[u16] = &[
         0, 1, 4,
         1, 2, 4,
         2, 3, 4,
+        5, 3, 2,
     ];
 }
 use unformatted::*;
